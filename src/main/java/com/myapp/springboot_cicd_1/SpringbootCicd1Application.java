39484008b1d213d8543e2ea368681c083aeea3d1
subhashis <subhashis.sarangi@gmail.com>
2024-04-05 06:56:56 +0530

    commit

--- src/main/java/com/myapp/springboot_cicd_1/SpringbootCicd1Application.java
+++ src/main/java/com/myapp/springboot_cicd_1/SpringbootCicd1Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootCicd1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootCicd1Application.class, args);
+		SpringApplication.run(SpringbootCicd1Application.class, args); 
 	}
 
 }
